e7c4907ad676c5b93f2cee255f44e80bd54d8dd9
jelysecalkum <jelyse.lauren@gmail.com>
2023-04-13 15:08:05 -0700

    added content from practice to main app

---
+++ my-app/client/src/Pages/about.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+import adopt from '../assets/images/whitedog-adopt.png';
+
+
+function About() {
+  return (
+    <Card style={{ width: '18rem', height: '18rem' }}>
+
+      <Card.Img variant="top" id="card1" src={adopt} />
+      <Card.Body>
+        <Card.Title>Card Title</Card.Title>
+        <Card.Text>
+          Looking for a new addition to your family?
+        </Card.Text>
+        <Button variant="primary">Go somewhere</Button>
+      </Card.Body>
+
+      <Card.Img variant="top" id="card1" src={adopt} />
+      <Card.Body>
+        <Card.Title>Card Title</Card.Title>
+        <Card.Text>
+          Looking for a new addition to your family?
+        </Card.Text>
+        <Button variant="primary">Go somewhere</Button>
+      </Card.Body>
+
+      <Card.Img variant="top" id="card1" src={adopt} />
+      <Card.Body>
+        <Card.Title>Card Title</Card.Title>
+        <Card.Text>
+          Looking for a new addition to your family?
+        </Card.Text>
+        <Button variant="primary">Go somewhere</Button>
+      </Card.Body>
+
+
+
+    </Card>
+
+    
+  );
+}
+
+  
+export default About;
---
+++ my-app/client/src/Pages/favedogs.js
@@ -0,0 +1,11 @@
+import React from 'react';
+  
+const Favedogs = () => {
+  return (
+    <div>
+      <h1>Saved/Favorites Dogs Here</h1>
+    </div>
+  );
+};
+  
+export default Favedogs;
