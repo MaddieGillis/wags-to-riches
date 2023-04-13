2a4abc7df7e8b13b0dd12e323efa14f21aa1f971
Clown Supreme <zachary.h.shedd@gmail.com>
2023-04-12 19:30:01 -0700

    Restructured files

--- my-app/server/models/User.js
+++ my-app/server/models/User.js
@@ -1,18 +1,53 @@
-const {Schema, model} = require("mongoose")
 
-const userSchema = new Schema(
+
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const { generateAuthToken } = require('../utils/check-auth');
+
+
+const userSchema = new mongoose.Schema(
     {
-        username:{
+        username: {
             type: String,
             required: true,
-            unique: true,
-            trim: true,
+            unique: [true, 'Username is in use.'],
         },
-        email:{
+        email: {
             type: String,
-            require: true,
-            unique: true,
-            match: [/.+@.+\..+/,"must match valid email address"],
+            required: true,
+            unique: [true, 'Email is in use.'],
         },
-    }
-)
+        avatar: {
+            type: String,
+            default: null
+        },
+        city: {
+            type: String,
+            default: null,
+        },
+        isAdmin: {
+            type: Boolean,
+            default: false,
+        },
+        password: {
+            type: String,
+            minlength: [8, 'Your minimum password length must be 8 characters long.'],
+            required: true,
+        },
+        token: {
+            type: String,
+        },
+    },
+    {timestamps: true }
+);
+
+userSchema.pre('save', async function (next) {
+    const salt = await bcrypt.genSalt(8);
+    this.password = await bcrypt.hash(this.password, salt);
+    this.token = generateAuthToken(this);
+    next();
+});
+
+
+module.exports = mongoose.model('User', userSchema);
