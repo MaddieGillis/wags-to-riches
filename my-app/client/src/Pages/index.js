23c212ca849fb9f0fdeede518f4949dc9e1890a9
jelysecalkum <jelyse.lauren@gmail.com>
2023-04-13 19:48:46 -0700

    deleted unused buttons

--- my-app/client/src/Pages/index.js
+++ my-app/client/src/Pages/index.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Home = () => {
   return (
     <div>
-      <h1>Welcome to GeeksforGeeks</h1>
+      <h1>I DON'T KNOW IF WE NEED THIS PAGE!!!</h1>
     </div>
   );
 };
