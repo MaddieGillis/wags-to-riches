3454feb0b0e38e8d3b841ba40072a3cb676d7096
jelysecalkum <jelyse.lauren@gmail.com>
2023-04-13 19:35:21 -0700

    fixed bugs to make code running

--- my-app/client/src/Pages/about.js
+++ my-app/client/src/Pages/about.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
-import adopt from '../assets/images/whitedog-adopt.png';
+import adopt from '../assets/images/whitedog-adopt.jpg';
 
 
 function About() {
--- my-app/client/src/Pages/donate.js
+++ my-app/client/src/Pages/donate.js
@@ -1,46 +1,11 @@
 import React from 'react';
-import Button from 'react-bootstrap/Button';
-import Card from 'react-bootstrap/Card';
-import adopt from '../assets/images/whitedog-adopt.png';
 
-
-function Donate() {
-  return (
-    <Card style={{ width: '18rem', height: '18rem' }}>
-
-      <Card.Img variant="top" id="card1" src={adopt} />
-      <Card.Body>
-        <Card.Title>Card Title</Card.Title>
-        <Card.Text>
-          Looking for a new addition to your family?
-        </Card.Text>
-        <Button variant="primary">Go somewhere</Button>
-      </Card.Body>
-
-      <Card.Img variant="top" id="card1" src={adopt} />
-      <Card.Body>
-        <Card.Title>Card Title</Card.Title>
-        <Card.Text>
-          Looking for a new addition to your family?
-        </Card.Text>
-        <Button variant="primary">Go somewhere</Button>
-      </Card.Body>
-
-      <Card.Img variant="top" id="card1" src={adopt} />
-      <Card.Body>
-        <Card.Title>Card Title</Card.Title>
-        <Card.Text>
-          Looking for a new addition to your family?
-        </Card.Text>
-        <Button variant="primary">Go somewhere</Button>
-      </Card.Body>
-
-
-
-    </Card>
-
-    
-  );
-}
+const Donate = () => {
+    return (
+      <div>
+        <h1>Mail us on feedback@geeksforgeeks.org</h1>
+      </div>
+    );
+  };
 
 export default Donate;
