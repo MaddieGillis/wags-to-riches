a5a64dbd2a4dc4ed5974164b4c1c9ea4745a9138
jelysecalkum <jelyse.lauren@gmail.com>
2023-04-16 15:49:26 -0700

    just trying some things out

--- client/src/Pages/home.js
+++ client/src/Pages/home.js
@@ -40,19 +40,19 @@ function About() {
 
     <>
 
-    <Container id="img-container" style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', paddingLeft: '4%' }}>
+    <Container id="img-container" style={{ display: 'flex'}}>
      <Card.Img id="big-logo" style={{ width: 400, height: 400, borderRadius: 400/2 }} src={logo}  />
       <Card.Body>
-        <Card.Text style={{ display: 'flex', justifyContent: 'left', alignItems: 'center' }}id="logo-text">
+        <Card.Text style={{ display: 'flex' }}id="logo-text">
           Ready to meet your new best friend?
         </Card.Text>
       </Card.Body>
       </Container>
 
 
-   <Container id="card-container" style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+   <Container id="card-container" style={{ display: 'flex' }}>
     Hello world!
-    <Card style={{ width: '18rem', height: '18rem' }}>
+    <Card>
  
       
 
@@ -65,7 +65,7 @@ function About() {
    Hello world!
     <Card.Img variant="top" id="card1" src={adopt} />
       <Card.Body>
-        <Card.Title>{a.name} {a.gender} {a.size} {a.type} {a.age}</Card.Title>
+        <Card.Title style={{ display: 'flex', alignItems: 'center' }}>{a.name} {a.gender} {a.size} {a.type} {a.age}</Card.Title>
         <p> {a.status} {a.imagePath}</p>
         <Button id="btn">Adopt Me!</Button>
       </Card.Body>
