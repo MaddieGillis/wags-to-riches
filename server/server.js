ce33529c78ab3116772b0210d3b8ab788041e3a7
MaddieGillis <112782694+MaddieGillis@users.noreply.github.com>
2023-04-17 16:24:43 -0700

    deploying

--- server/server.js
+++ server/server.js
@@ -36,7 +36,7 @@ const startApolloServer = async (typeDefs, resolvers) => {
   
   db.once('open', () => {
     app.listen(PORT, () => {
-      console.log(`API server running on port ${PORT}!`);
+      console.log(`API server running on port ${PORT}!🐶🐶🐶`);
       console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
     })
   })
